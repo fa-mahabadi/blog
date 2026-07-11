3c62a7ffe3cd875c02997c7feabb7fe99aab27cf
linux <fa.mahabadi@yahoo.com>
2026-07-11 17:43:39 +0330

    ADD| share post by email

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,10 @@
-from .views import post_list,post_detail
+from .views import post_list,post_detail,PostListView,post_share
 from django.urls import path
 
 app_name="blog"
 urlpatterns=[
-    path("",post_list,name="post_list"),
-    path("<int:year>/<int:month>/<int:day>/<slug:post>/",post_detail,name="post_detail")
+    path("",PostListView.as_view(),name="post_list"),
+    # path("",post_list,name="post_list"),
+    path("<int:year>/<int:month>/<int:day>/<slug:post>/",post_detail,name="post_detail"),
+    path("<int:post_id>/share_post/",post_share,name="post_share"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,45 @@ from django.shortcuts import render,get_object_or_404
 from django.http import Http404
 from .models import Post
 from django.core.paginator import EmptyPage, Paginator,PageNotAnInteger
+from django.core.mail import send_mail
+from django.views.generic import ListView
+from .forms import EmailPostForm
+
+class PostListView(ListView):
+    template_name='blog/post_list.html'
+    context_object_name='posts'
+    queryset=Post.published.all()
+    paginate_by=3
+
+
+def post_share(request,post_id):
+    post=get_object_or_404(Post,status=Post.Status.PUBLISH,id=post_id)
+    sent=False
+    if request.method=="POST":
+        form=EmailPostForm(request.POST)
+        if form.is_valid():
+            cd=form.cleaned_data
+            post_url=request.build_absolute_uri(post.get_absolute_url())
+            subject=(
+                f"{cd['name']} ({cd['email']}) "
+                f"recommends you read {post.title}"
+                )
+            message=(
+                f"read {post.title} at {post_url}\n\n"
+                f"{cd['name']}\'s comments:{cd['comments']}"
+            )
+            send_mail(subject=subject,message=message,from_email=None,recipient_list=[cd['to']])
+            sent=True
+           
+    else:
+        form=EmailPostForm()
+    return render(request,'blog/share.html',{'form':form,'post':post,'sent':sent})
+        
+
+
+
+
+
 
 def post_list(request):
     post_list=Post.objects.all()
