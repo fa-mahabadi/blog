7bfbf9fdb9b512eb070428c7429864cb0db5806f
linux <fa.mahabadi@yahoo.com>
2026-07-18 19:17:43 +0330

    ADD| template filter tag

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,16 +1,33 @@
 from django import template
 from ..models import Post
 from django.db.models import Count
+import markdown
+from django.utils.safestring import mark_safe
 
-register=template.Library()
+# custom template tags
+
+register = template.Library()
+
+
 @register.simple_tag
 def total_posts():
     return Post.published.count()
 
-@register.inclusion_tag('blog/lastest_post.html')
+
+@register.inclusion_tag("blog/lastest_post.html")
 def show_lastest_posts():
-    lastest_posts=Post.published.order_by('-publish')[:5]
-    return {'lastest_posts':lastest_posts}
+    lastest_posts = Post.published.order_by("-publish")[:5]
+    return {"lastest_posts": lastest_posts}
+
+
 @register.simple_tag
 def get_most_commented_post():
-    return Post.published.annotate(total_comments=Count('comments')).order_by('-total_comments')[:3]
+    return Post.published.annotate(total_comments=Count("comments")).order_by(
+        "-total_comments"
+    )[:3]
+
+
+# custom template filters
+@register.filter(name="markdown")
+def markdown_format(txt):
+    return mark_safe(markdown.markdown(txt))
