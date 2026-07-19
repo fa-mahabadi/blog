d3b084d47a719bcc19a80c5f6ea5a41edd7150f6
linux <fa.mahabadi@yahoo.com>
2026-07-19 15:54:27 +0330

    ADD| sitemap

--- config/settings.py
+++ config/settings.py
@@ -30,13 +30,15 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
-
+SITE_ID=1
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.sites',
+    'django.contrib.sitemaps',
     'django.contrib.staticfiles',
     # third party
     'taggit',
